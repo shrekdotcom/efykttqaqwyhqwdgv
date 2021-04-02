739efd254c2cae102e594c810ee0ee872f5277e1
shreklikescode <fathershreklovesyou@gmail.com>
2021-04-02 19:24:57 +0000

    ban command!

--- index.js
+++ index.js
@@ -55,6 +55,39 @@ client.on("message", message => {
 			.setFooter("Make sure you are secretive or you will get reported.")
 		message.channel.send(embed)
 	}
-})
+if (message.content.startsWith(">kick")) {
+
+    if (!message.member.roles.find("name", "Admin"))
+        return;
+    // Easy way to get member object though mentions.
+    var member = message.mentions.members.first();
+    // Kick
+    member.kick().then((member) => {
+        // Successmessage
+        message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+    }).catch(() => {
+        // Failmessage
+        message.channel.send("Access Denied");
+    });
+}
+});
+client.on("message", (message) => {
+    if (message.content.startsWith(">ban")) {
+
+        if (!message.member.roles.find("name", "Admin"))
+            return;
+
+        // Easy way to get member object though mentions.
+        var member = message.mentions.members.first();
+        // ban
+        member.ban().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
+        }).catch(() => {
+            // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
 
 client.login(process.env.token);
