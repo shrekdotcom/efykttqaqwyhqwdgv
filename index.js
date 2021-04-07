a92dce1c5de700e2f2d65f9babc0eb3f56e360e6
shreklikescode <>
2021-04-07 20:17:49 +0000

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 app.get("/", (req, res) => {
-	res.send("hello hell!")
+	res.send("how did u find this lol")
 })
 
 app.listen(3000, () => {
